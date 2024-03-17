88eb4733c932564356813e0e2c554ae710e9e6ae
KevinPgn <kevin.pigeon2810@gmail.com>
2024-03-17 15:43:39 +0100

    correction footer

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,5 @@
 import { Blog } from "../components/Home/Blog/Blog"
 import { Features } from "../components/Home/Featured/Features"
-import { Footer } from "../components/Home/Footer/Footer"
 import { Hero } from "../components/Home/Hero/Hero"
 import { Infos } from "../components/Home/Informations/Infos"
 import { Parallax } from "../components/Home/Parallax/Parallax"
@@ -12,6 +11,5 @@ export const Home = () => {
     <Parallax />
     <Blog />
     <Infos />
-    <Footer />
   </>
 }
