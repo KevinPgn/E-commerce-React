77eead02b52256c9bbf64ef1666d29f84b53cac8
KevinPgn <kevin.pigeon2810@gmail.com>
2024-03-19 14:50:08 +0100

    add menu burger

--- src/components/Home/Navbar/Navbar.tsx
+++ src/components/Home/Navbar/Navbar.tsx
@@ -6,38 +6,52 @@ import {Link} from 'react-router-dom'
 import { useInventaire } from "../../../store/Inventaire";
 import { useShop } from "../../../store/Shop";
 import { useFavories } from "../../../store/Favories";
+import { useState } from "react";
+import { CiMenuBurger } from "react-icons/ci";
 
 export const Navbar = () => {
   const inventaire = useInventaire(state => state.inventaire)
   const favories = useFavories(state => state.favories)
   const toggleCart = useShop(state => state.toggleCart)
 
+  const [toggleLink, setToggleLink] = useState(false)
 
-  return <>
-    <header>
-      <nav>
-        <div className="left">
-          <div className="logo">Shopp<span>Hub</span></div>
-        </div>
-        <div className="mid">
-          <Link to="/">Home</Link>
-          <Link to="/products">Products</Link>
-          <Link to="/about">About</Link>
-        </div>
-        <div className="right">
-          <Link to='/favories' className="heart">
-            <span>{favories.length}</span>
-            <CiHeart className="icon"/>
-          </Link>
-          <div className="cart">
-            <span>{inventaire.length}</span>
-            <CiShoppingCart className="icon" onClick={() => toggleCart()}/>
+
+    return <>
+      <header>
+        <nav>
+          <div className="left">
+            <div className="logo">Shopp<span>Hub</span></div>
           </div>
-          <div className="profile">
-            <IoPersonOutline className="icon"/>
+          <div className="mid">
+            <Link to="/">Home</Link>
+            <Link to="/products">Products</Link>
+            <Link to="/about">About</Link>
           </div>
-        </div>
-      </nav>
-    </header>
-  </>
+          <div className="right">
+            <Link to='/favories' className="heart">
+              <span>{favories.length}</span>
+              <CiHeart className="icon heart"/>
+            </Link>
+            <div className="cart">
+              <span>{inventaire.length}</span>
+              <CiShoppingCart className="icon carticon" onClick={() => toggleCart()}/>
+            </div>
+            <div className="profile">
+              <IoPersonOutline className="icon"/>
+            </div>
+            <div className="burger" onClick={() => setToggleLink(!toggleLink)}>
+              <CiMenuBurger className="icon"/>
+            </div>
+          </div>
+        </nav>
+      </header>
+      {toggleLink && (
+          <div className={toggleLink ? "menu-link active":"menu-link"}>
+            <Link to="/" onClick={() => setToggleLink(false)}>Home</Link>
+            <Link to="/products" onClick={() => setToggleLink(false)}>Products</Link>
+            <Link to="/about" onClick={() => setToggleLink(false)}>About</Link>
+          </div>
+      )}
+    </>
 }
